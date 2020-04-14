97115cbf955b71f2d218b30e92ba3796284c147b
Meearlyam <shavela99@gmail.com>
2020-04-14 12:17:15 +0300

    Configured Spring Boot to use Java 8 Date/Time converters and UTC Timezone

--- src/main/java/by/meearlyam/dentistry/DentistryApplication.java
+++ src/main/java/by/meearlyam/dentistry/DentistryApplication.java
@@ -2,9 +2,23 @@ package by.meearlyam.dentistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {
+		DentistryApplication.class,
+		Jsr310JpaConverters.class
+})
 public class DentistryApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DentistryApplication.class, args);
