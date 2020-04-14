6518b651927e2e2053baf3c280be5de53831616b
Meearlyam <shavela99@gmail.com>
2020-04-14 11:44:06 +0300

    Add project basis

---
+++ src/test/java/by/meearlyam/dentistry/DentistryApplicationTests.java
@@ -0,0 +1,13 @@
+package by.meearlyam.dentistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
