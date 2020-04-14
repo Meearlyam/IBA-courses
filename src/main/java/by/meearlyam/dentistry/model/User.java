68975ff40b121e1a0f4f6b6a0311ded41d454567
Meearlyam <shavela99@gmail.com>
2020-04-14 12:45:19 +0300

    created domain models

---
+++ src/main/java/by/meearlyam/dentistry/model/User.java
@@ -0,0 +1,98 @@
+package by.meearlyam.dentistry.model;
+
+import by.meearlyam.dentistry.model.audit.DateAudit;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "login"
+        })
+})
+public class User extends DateAudit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 30)
+    private String name;
+
+    @NotBlank
+    @Size(max = 30)
+    private String surname;
+
+    @NotBlank
+    @Size(max = 20)
+    private String login;
+
+    @NotBlank
+    @Size(max = 20)
+    private String password;
+
+    @NotBlank
+    private boolean isAdmin;
+
+    public User() {
+
+    }
+
+    public User(String name, String surname, String login, String password, boolean isAdmin) {
+        this.name = name;
+        this.surname = surname;
+        this.login = login;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+}
